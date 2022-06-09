162a63516b736373055489d2f569ef50a66a0eda
Ashish <aashish.bhujel@gmail.com>
2022-06-09 14:35:40 +0545

    Icon added for weather #2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import { WiThermometer, WiHumidity, WiCloudUp } from "react-icons/wi";
 import { BiMap } from "react-icons/bi";
-import weather from "./assets/clear.svg";
 import axios from "axios";
- 
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY;
   const [data, setData] = useState({});
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState("");
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       axios.get(url).then((response) => {
         setData(response.data);
+        setLocation("");
       });
     }
   };
@@ -38,54 +37,62 @@ function App() {
             onKeyPress={searchLocation}
             spellheck="false"
           />
+
           <div className="separator"></div>
         </section>
-        <section className="weather-part">
-          <img src={weather} alt="Weather Icon" />
-          <div className="temp">
-            <span className="numb">
-              {data.main ? data.main.temp.toFixed() : null}
-            </span>
-            <span className="deg">°</span>C
-          </div>
-          <div className="weather">
-            {data.weather ? data.weather[0].main : null}
-          </div>
-          <div className="location">
-            <i>
-              <BiMap />
-            </i>
-            <span>{data.name}</span>
-          </div>
-          <div className="bottom-details">
-            <div className="column feels">
+        {data.main ? (
+          <section className="weather-part">
+            <img
+              src={`./assets/${data.weather ? data.weather[0].icon : null}.svg`}
+              alt="Weather Icon"
+            />
+            <div className="temp">
+              <span className="numb">
+                {data.main ? data.main.temp.toFixed() : null}
+              </span>
+              <span className="deg">°</span>C
+            </div>
+            <div className="weather">
+              {data.weather ? data.weather[0].main : null}
+            </div>
+            <div className="location">
               <i>
-                <WiThermometer />
+                <BiMap />
               </i>
-              <div className="details">
-                <div className="temp">
-                  <span className="numb">
-                    {data.main ? data.main.feels_like.toFixed() : null}{" "}
-                  </span>
-                  <span className="deg">°</span>C
+              <span>{data.name}</span>
+            </div>
+            <div className="bottom-details">
+              <div className="column feels">
+                <i>
+                  <WiThermometer />
+                </i>
+                <div className="details">
+                  <div className="temp">
+                    <span className="numb">
+                      {data.main ? data.main.feels_like.toFixed() : null}{" "}
+                    </span>
+                    <span className="deg">°</span>C
+                  </div>
+                  <p>Feels like </p>
                 </div>
-                <p>Feels like </p>
+              </div>
+              <div className="column humidity">
+                <i>
+                  <WiHumidity />
+                </i>
+                <div className="details">
+                  <span className="numb">
+                    {" "}
+                    {data.main ? data.main.humidity.toFixed() : null}
+                  </span>
+                  <p> Humidity</p>
+                </div>
               </div>
             </div>
-            <div className="column humidity">
-              <i>
-                <WiHumidity />
-              </i>
-              <div className="details">
-                <span className="numb">
-                  {" "}
-                  {data.main ? data.main.humidity.toFixed() : null}
-                </span>
-                <p> Humidity</p>
-              </div>
-            </div>
-          </div>
-        </section>
+          </section>
+        ) : (
+          " "
+        )}
       </div>
     </div>
   );
